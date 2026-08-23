7168fba68c51499ad1534f2c4630dce1838720fd
Anh Tran <anh.tran.harris@gmail.com>
2026-08-23 01:09:20 -0500

    Bump carousel polished rim renderer to edge-8

--- motion-policy.js
+++ motion-policy.js
@@ -31,7 +31,7 @@
 
   if (!document.querySelector('script[data-carousel-edge-fx]')) {
     const carouselFx = document.createElement('script');
-    carouselFx.src = 'carousel-edge-fx.js?v=edge-7';
+    carouselFx.src = 'carousel-edge-fx.js?v=edge-8';
     carouselFx.dataset.carouselEdgeFx = 'true';
     carouselFx.async = false;
     document.head.appendChild(carouselFx);
